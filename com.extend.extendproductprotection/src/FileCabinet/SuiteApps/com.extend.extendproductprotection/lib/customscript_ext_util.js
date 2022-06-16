ab2930a93677eb12c5d547a1d044a1c7444d2bf4
amillerExtend <85582815+amillerExtend@users.noreply.github.com>
2022-06-16 09:01:05 -0500

    order creation/response handling

--- com.extend.extendproductprotection/src/FileCabinet/SuiteApps/com.extend.extendproductprotection/lib/customscript_ext_util.js
+++ com.extend.extendproductprotection/src/FileCabinet/SuiteApps/com.extend.extendproductprotection/lib/customscript_ext_util.js
@@ -170,7 +170,6 @@ define([
             return stPurchasePrice;
         };
         exports.getSalesOrderItemInfo = function (objSalesOrderRecord) {
-
             //////////////////////////SUPPORT FUNCTIONS///////////////////////////
             var stLineCount = objSalesOrderRecord.getLineCount({ sublistId: 'item' });
 
